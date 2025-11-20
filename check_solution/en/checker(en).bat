14733af82f2ac147fae90d1372b945ac3b3ec986
JimmyLee16 <120309277+JimmyLee16@users.noreply.github.com>
2025-11-20 15:40:49 +0700

    Update checker(en).bat

--- check_solution/en/checker(en).bat
+++ check_solution/en/checker(en).bat
@@ -1,4 +1,4 @@
 @echo off
 
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Checksolution_gui(vie).ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Checksolution_gui(en).ps1"
 pause
