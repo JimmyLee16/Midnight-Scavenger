0ab25f5d797678b2fd2982aff4764299d214658c
JimmyLee16 <120309277+JimmyLee16@users.noreply.github.com>
2025-11-20 23:45:39 +0700

    Delete solution_donation/solution_donation.bat

--- solution_donation/solution_donation.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo.
-echo ===========================================
-echo Scavenger Donation Flow (v3.1)
-echo ===========================================
-echo.
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0solution_transfer_manual_gui.ps1"
-pause
