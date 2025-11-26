b4dc044bb4bdfa98e6179ad465bee48f942a558d
JimmyLee16 <120309277+JimmyLee16@users.noreply.github.com>
2025-11-26 13:18:43 +0700

    Add files via upload

---
+++ solution_donation/v1.3/solution_transfer_manual_gui.bat
@@ -0,0 +1,8 @@
+@echo off
+echo.
+echo ===========================================
+echo Scavenger Donation Flow (v3.1)
+echo ===========================================
+echo.
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0solution_transfer_manual_gui_v3.ps1"
+pause
